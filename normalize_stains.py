c3cc60a28f5b969bf9734c7b9644177da9480f9d
root <benzelongji@gmail.com>
2025-11-10 16:49:37 +0900

    Code to normalize images

--- normalize_stains.py
+++ normalize_stains.py
@@ -1,34 +1,51 @@
-import staintools
 import os
 import glob
-import cv2
+import random
+from numpy.linalg import LinAlgError
+from tiatoolbox.tools import stainnorm
+from tiatoolbox.utils.misc import imread, imwrite
 
-# 基準となる画像を設定する
-target = staintools.read_image("/workspace/inhouse-vqvae/VQVAE/data/preprocessed/27527/slice_0/patch_2_70.jpeg")
-target = staintools.LuminosityStandardizer.standardize(target)
+target = imread("/workspace/inhouse-vqvae/VQVAE/data/preprocessed/27527/slice_0/patch_2_70.jpeg")
+# target = staintools.LuminosityStandardizer.standardize(target)
 
-# ノーマライザーの定義
-# Vahadaneを指定している
-normalizer = staintools.StainNormalizer(method='vahadane')
+normalizer = stainnorm.VahadaneNormalizer()
 normalizer.fit(target)
 
-# 入力画像と出力画像のディレクトリを指定
 INPUT_DIR = "/workspace/inhouse-vqvae/VQVAE/data/preprocessed"
 OUTPUT_DIR = "/workspace/inhouse-vqvae/VQVAE/data/normalize_vahadane"
 
-search_pattern = os.path.join(INPUT_DIR, "**", "*.jpeg")
-image_paths = glob.glob(search_pattern, recursive=True)
+dirs = os.listdir(INPUT_DIR)
+sorted_dirs = sorted(dirs)
+i = 0
+for sorted_dir in sorted_dirs:
+    if i <= 2:
+        # 途中から再開するためのループ、あとで消して
+        i+=1
+    else:
+        print(sorted_dir)
+        search_pattern = os.path.join(INPUT_DIR, sorted_dir, "**", "*.jpeg")
+        image_paths = glob.glob(search_pattern, recursive=True)
+        # random.seed(42)
+        # random.shuffle(image_paths)
+        image_paths = sorted(image_paths)
+        print(f"{sorted_dir} から {len(image_paths)} 枚の画像をノーマライズします")
+        output_dir = os.path.join(OUTPUT_DIR, sorted_dir)
+        os.makedirs(output_dir, exist_ok=True)
 
-for path in image_paths:
-    relative_path = os.path.relpath(path, INPUT_DIR)
-    output_path = os.path.join(OUTPUT_DIR, relative_path)
-    output_dir = os.path.dirname(output_path)
+        for path in image_paths:
+            try:
+                to_transform = imread(path)
+                # to_transform = staintools.LuminosityStandardizer.standardize(to_transform)
 
-    os.makedirs(output_dir, exist_ok=True)
+                transformed = normalizer.transform(to_transform)
+                
+                file_name = os.path.basename(path)
+                output_path = os.path.join(output_dir, file_name)
+                imwrite(output_path, transformed)
+            except LinAlgError as e:
+                print(f"警告: {path} のSVDが収束せずスキップします。エラー: {e}")
+                continue
+            except Exception as e:
+                print(f"その他のエラー: {path} をスキップします。エラー: {e}")
+                continue
 
-    to_transform = staintools.read_image(path)
-    to_transform = staintools.LuminosityStandardizer.standardize(to_transform)
-
-    transformed = normalizer.transform(to_transform)
-
-    cv2.imwrite(output_path, transformed)
